33d45a0237bd2a13b0074ceff25b1fbc8a036bde
Ebun-nofiu <ebunnofiu@gmail.com>
2023-04-23 09:35:40 -0700

    Task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+*main - prints the alphabet in lowercase
+*
+*Return: void
+*/
+
+int main(void)
+{
+	int no;
+
+	for (no = 0; no <= 9; no++)
+		printf("%d", no);
+	printf("\n");
+	return (0);
+}
