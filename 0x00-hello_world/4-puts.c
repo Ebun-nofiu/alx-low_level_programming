91b8100f46a382653f4818b41623851b8ed141dc
Ebun-nofiu <ebunnofiu@gmail.com>
2023-04-23 13:20:34 -0700

    Task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ *main - it is the main function
+ *
+ *Return: usually zero
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
