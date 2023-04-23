f3ab35618b90fe2fbdb48622b3da014191912138
Ebun-nofiu <ebunnofiu@gmail.com>
2023-04-23 13:22:25 -0700

    Task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - this is the main function
+ *
+ * Return: it replies the integer function
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
